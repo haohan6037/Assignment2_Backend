c2230aea935e800b14a4570853aa96c22da3a244
haohan6037 <haohan6037@gmail.com>
2025-04-12 13:42:15 +1200

    get post detail

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import CreatePostView
+from .views import CreatePostView, PostDetailView
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('posts/create/', CreatePostView.as_view(), name='create-post'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
